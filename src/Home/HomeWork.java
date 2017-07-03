b69cfbdadb0fa251497641bce4b864e10403b21c
opimahsergei <opimahsergei@gmail.com>
2017-07-03 23:00:37 +0300

    создал проект

---
+++ src/Home/HomeWork.java
@@ -0,0 +1,11 @@
+package Home;
+
+/**
+ * Created by User on 03.07.2017.
+ */
+public class HomeWork {
+    public static void main(String[]args){
+        System.out.println("Как ты проведешь лето 2017");
+    }
+
+}
